d4e0825dc08f1286da1c014590c5f22173b89c10
Angelo Bisquera <angelo.bisquera@ph.c3-interactive.com>
2026-01-20 14:27:59 +0800

    fix migrate

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -9,7 +9,7 @@ import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import AuthBase from '@/layouts/AuthLayout.vue';
 import { login } from '@/routes';
-import { store } from '@/routes/register';
+// import { store } from '@/routes/register';
 </script>
 
 <template>
@@ -18,7 +18,7 @@ import { store } from '@/routes/register';
         description="Enter your details below to create your account"
     >
         <Head title="Register" />
-
+<!-- 
         <Form
             v-bind="store.form()"
             :reset-on-success="['password', 'password_confirmation']"
@@ -104,6 +104,6 @@ import { store } from '@/routes/register';
                     >Log in</TextLink
                 >
             </div>
-        </Form>
+        </Form> -->
     </AuthBase>
 </template>
